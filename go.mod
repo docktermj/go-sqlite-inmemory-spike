3868a9d8e45ecfb8365128a22699f0e1da6715ec
Michael Dockter <michael@dockter.com>
2024-11-08 11:10:01 -0500

    #1 Working schema creation

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.0
 toolchain go1.23.1
 
 require (
+	github.com/mattn/go-sqlite3 v1.14.24
 	github.com/senzing-garage/go-cmdhelping v0.3.1
 	github.com/spf13/cobra v1.8.1
 	github.com/spf13/viper v1.19.0
@@ -31,8 +32,8 @@ require (
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c // indirect
-	golang.org/x/sys v0.26.0 // indirect
-	golang.org/x/text v0.19.0 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,12 +3,100 @@
 package main
 
 import (
-	"log"
+	"bufio"
+	"context"
+	"database/sql"
+	"database/sql/driver"
+	"fmt"
+	"net/url"
+	"os"
+	"path/filepath"
 
-	"github.com/senzing-garage/go-sqlite-inmemory-spike/cmd"
+	sqlite "github.com/mattn/go-sqlite3"
+)
+
+const (
+	DatabaseURL = "sqlite3://na:na@/MYPRIVATE_DB?mode=memory&cache=shared"
+	SQLfile     = "/opt/senzing/er/resources/schema/szcore-schema-sqlite-create.sql"
 )
 
 func main() {
-	log.SetFlags(0)
-	cmd.Execute()
+
+	var (
+		scanLine    = 0
+		scanFailure = 0
+		ctx         = context.TODO()
+		name        = ""
+	)
+
+	// Open a connection to the SQLite database.
+
+	parsedURL, err := url.Parse(DatabaseURL)
+	panicOnError(err)
+	connectionString := parsedURL.Path
+	if len(parsedURL.RawQuery) > 0 {
+		connectionString = fmt.Sprintf("file:%s?%s", connectionString, parsedURL.Query().Encode())
+	}
+	databaseConnector := &Sqlite{
+		ConnectionString: connectionString,
+	}
+	database := sql.OpenDB(databaseConnector)
+
+	// Write to SQLite database from file.
+
+	sqlFile := filepath.Clean(SQLfile)
+	file, err := os.Open(sqlFile)
+	panicOnError(err)
+	defer func() {
+		if err := file.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		scanLine++
+		sqlText := scanner.Text()
+		_, err := database.ExecContext(ctx, sqlText)
+		if err != nil {
+			scanFailure++
+		}
+	}
+	panicOnError(scanner.Err())
+
+	// Verify database schema installed
+
+	sqlRows, err := database.Query("SELECT name FROM sqlite_master WHERE type='table';")
+	panicOnError(err)
+	defer sqlRows.Close()
+
+	for sqlRows.Next() {
+		err := sqlRows.Scan(&name)
+		if err != nil {
+			fmt.Printf(">>>>> error: %v\n", err)
+		}
+		fmt.Printf(">>>>> name: %s\n", name)
+	}
+
+}
+
+func panicOnError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// ----------------------------------------------------------------------------
+// Type Sqlite
+// ----------------------------------------------------------------------------
+
+type Sqlite struct {
+	ConnectionString string
+}
+
+func (connector *Sqlite) Connect(_ context.Context) (driver.Conn, error) {
+	return connector.Driver().Open(connector.ConnectionString)
+}
+
+func (connector *Sqlite) Driver() driver.Driver {
+	return &sqlite.SQLiteDriver{}
 }
