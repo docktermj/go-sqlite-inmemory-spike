75365bc15d96f251ce1df452ab138eee04ba4147
Michael Dockter <michael@dockter.com>
2024-11-08 14:24:37 -0500

    #1 Savepoint

--- main.go
+++ main.go
@@ -11,32 +11,39 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+	"time"
 
 	sqlite "github.com/mattn/go-sqlite3"
 	"github.com/senzing-garage/go-sdk-abstract-factory/szfactorycreator"
 )
 
 const (
-	DatabaseURL = "sqlite3://na:na@/MYPRIVATE_DB?mode=memory&cache=shared"
+	// DatabaseURL = "sqlite3://na:na@/MYPRIVATE_DB?mode=memory&cache=shared"
+	// DatabaseURL = "sqlite3://na:na@/tmp/sqlite/mike.db?mode=memory&cache=shared"
+
+	DatabaseURL = "sqlite3://na:na@/tmp/sqlite/mike.db"
 	SQLfile     = "/opt/senzing/er/resources/schema/szcore-schema-sqlite-create.sql"
 )
 
 func main() {
 
 	var (
-		scanLine    = 0
-		scanFailure = 0
-		ctx         = context.TODO()
-		name        = ""
+		scanLine        = 0
+		scanFailure     = 0
+		ctx             = context.TODO()
+		senzingSettings = `{"PIPELINE":{"CONFIGPATH":"/etc/opt/senzing","RESOURCEPATH":"/opt/senzing/er/resources","SUPPORTPATH":"/opt/senzing/data"},"SQL":{"CONNECTION": "` + DatabaseURL + `"}}`
 	)
 
 	// Open a connection to the SQLite database.
 
 	parsedURL, err := url.Parse(DatabaseURL)
-	panicOnError(err)
+	onErrorPanic(err)
+	// connectionString := parsedURL.Path[1:]
 	connectionString := parsedURL.Path
 	if len(parsedURL.RawQuery) > 0 {
-		connectionString = fmt.Sprintf("file:%s?%s", connectionString, parsedURL.Query().Encode())
+		queryParameters := parsedURL.Query().Encode()
+		// escapedQueryParameters := url.QueryEscape(queryParameters)
+		connectionString = fmt.Sprintf("file:%s?%s", connectionString, queryParameters)
 	}
 	fmt.Printf(">>>>> connectionString: %s\n", connectionString)
 	databaseConnector := &Sqlite{
@@ -48,7 +55,7 @@ func main() {
 
 	sqlFile := filepath.Clean(SQLfile)
 	file, err := os.Open(sqlFile)
-	panicOnError(err)
+	onErrorPanic(err)
 	defer func() {
 		if err := file.Close(); err != nil {
 			panic(err)
@@ -63,62 +70,71 @@ func main() {
 			scanFailure++
 		}
 	}
-	panicOnError(scanner.Err())
+	onErrorPanic(scanner.Err())
 
 	// Verify database schema installed
 
-	sqlRows, err := database.Query("SELECT name FROM sqlite_master WHERE type='table';")
-	panicOnError(err)
-	defer sqlRows.Close()
-
-	for sqlRows.Next() {
-		err := sqlRows.Scan(&name)
-		if err != nil {
-			fmt.Printf(">>>>> error: %v\n", err)
-		}
-		fmt.Printf(">>>>> table name: %s\n", name)
-	}
+	listTable(database)
 
 	// Add Senzing Configuration.
 
-	senzingSettings := `{"PIPELINE":{"CONFIGPATH":"/etc/opt/senzing","RESOURCEPATH":"/opt/senzing/er/resources","SUPPORTPATH":"/opt/senzing/data"},"SQL":{"CONNECTION": "` + DatabaseURL + `"}}`
 	fmt.Printf(">>>>> senzingSettings: %s\n", senzingSettings)
 	szAbstractFactory, err := szfactorycreator.CreateCoreAbstractFactory("test", senzingSettings, 0, 0)
-	panicOnError(err)
-
-	// szConfig, err := szAbstractFactory.CreateSzConfig(ctx)
-	// panicOnError(err)
+	onErrorPanic(err)
 
 	szConfigManager, err := szAbstractFactory.CreateSzConfigManager(ctx)
-	if err != nil {
-		fmt.Printf(">>>>> Error during CreateSzConfigManager: %s\n", err.Error())
-	}
-	configID, err := szConfigManager.GetDefaultConfigID(ctx)
-	if err != nil {
-		fmt.Printf(">>>>> Error during GetDefaultConfigID: %s\n", err.Error())
-	}
+	onErrorLog(err, "szAbstractFactory.CreateSzConfigManager")
+	oldConfigID, err := szConfigManager.GetDefaultConfigID(ctx)
+	onErrorLog(err, "szConfigManager.GetDefaultConfigID")
 
-	fmt.Printf(">>>>> configID: %d\n", configID)
+	fmt.Printf(">>>>> Old Configuration id: %d\n", oldConfigID)
+
+	szConfig, err := szAbstractFactory.CreateSzConfig(ctx)
+	onErrorPanic(err)
+	configHandle, err := szConfig.CreateConfig(ctx)
+	onErrorLog(err, "szConfig.CreateConfig")
+	configStr, err := szConfig.ExportConfig(ctx, configHandle)
+	onErrorLog(err, "szConfig.ExportConfig")
+	configComments := fmt.Sprintf("Created at %s", time.Now().Format(time.RFC3339Nano))
+	newConfigID, err := szConfigManager.AddConfig(ctx, configStr, configComments)
+	onErrorLog(err, "szConfigManager.AddConfig")
+	err = szConfigManager.SetDefaultConfigID(ctx, newConfigID)
+	onErrorLog(err, "szConfigManager.SetDefaultConfigID")
+
+	fmt.Printf(">>>>> New Configuration id: %d\n", newConfigID)
 
 	// Verify database is still available.
 
-	sqlRows, err = database.Query("SELECT name FROM sqlite_master WHERE type='table';")
-	panicOnError(err)
+	listTable(database)
+
+}
+
+func onErrorLog(err error, message string) {
+	if err != nil {
+		fmt.Printf(">>>>> Error: %s; error: %s\n", message, err.Error())
+	}
+}
+
+func onErrorPanic(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func listTable(database *sql.DB) {
+	var (
+		name = ""
+	)
+	sqlRows, err := database.Query("SELECT name FROM sqlite_master WHERE type='table';")
+	onErrorPanic(err)
 	defer sqlRows.Close()
 
 	for sqlRows.Next() {
 		err := sqlRows.Scan(&name)
 		if err != nil {
-			fmt.Printf(">>>>> error: %v\n", err)
+			onErrorLog(err, "sqlRows.Next()")
 		}
 		fmt.Printf(">>>>> table name: %s\n", name)
-	}
-
-}
-
-func panicOnError(err error) {
-	if err != nil {
-		panic(err)
 	}
 }
 
